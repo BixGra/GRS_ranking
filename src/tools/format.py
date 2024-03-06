6a9ce4ed7743a7c391176c11ded5fe04d226c71f
BixGra <bixente.grandjean@gmail.com>
2024-03-07 00:31:56 +0100

    [+] Improved HTML formating

--- src/tools/format.py
+++ src/tools/format.py
@@ -2,8 +2,8 @@ from datetime import datetime
 
 
 def ranking_row(rank: int, name: str, country: str, team: str, points: int) -> str:
-    return f"""<tr class="bg-color-light-gray">\
-<td>{rank+1}</td>\
+    return f"""<tr class="ranking-row">\
+<td class="text-center">{rank+1}</td>\
 <td class="text-center">{name}</td>\
 <td class="text-center">{country}</td>\
 <td class="text-center">{team}</td>\
@@ -12,15 +12,15 @@ def ranking_row(rank: int, name: str, country: str, team: str, points: int) -> s
 
 
 def tournaments_row(tournament_id: int, name: str, ts: int) -> str:
-    return f"""<tr class="bg-color-light-gray">\
+    return f"""<tr class="tournament-row">\
 <td><a href="javascript:getResults({tournament_id}, '{name}')">{name}</a></td>\
 <td class="text-center">{datetime.strptime(str(ts), '%Y%m%d').date()}</td>\
 </tr>"""
 
 
 def results_row(rank: int, name: str, points: int) -> str:
-    return f"""<tr class="bg-color-light-gray">\
-<td>{rank}</td>\
+    return f"""<tr class="result-row">\
+<td class="text-center">{rank}</td>\
 <td class="text-center">{name}</td>\
 <td class="text-center">{points}</td>\
 </tr>"""
